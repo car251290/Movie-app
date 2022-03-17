690f138dadbc98b3909094d04ffbc0a4b94dd0dc
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2022-03-17 16:11:51 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,42 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
-import "./App.css";
+//import axios from 'react';
+import './App.css'
 import "./MovieCard";
 import MovieCard from "./MovieCard";
 
 //c728dd20
-const API_URL = "http://www.omdbapi.com?apikey=c728dd20";
+const API_URL = `http://www.omdbapi.com/?i=tt3896198&apikey=c728dd20`;
 
 
 const App = () => {
     const [movies,setMovies]= useState([]);
     const [searchterm, setsearch]= useState()
 
-    const searchMovies = async(title) => {
-        const response = await fetch(`${API_URL}&s=${title}`);
+ //   const searchMovies = async(title) => {
+ //       const response = await fetch(`${API_URL}&s=${title}`);
 
-        const data = await response.json();
+//        const data = await response.json();
+//       setMovies(data.Search);
+//        console.log(data);
+//    }
+
+   // useEffect( () => {
+     //   searchMovies('superman')
+
+
+    //},[]);
+
+    useEffect(()=>{
+     const searchMovies = async(title)=> {
+       const response = await fetch(`${API_URL}&s=${title}`);
+       const data = await response.json();
        setMovies(data.Search);
-        //console.log(data);
-    }
-
-    useEffect( () => {
-        searchMovies('spiderman')
-
-
+       console.log(data);
+     }
+     searchMovies('Batman')
     },[]);
+   
     
     return(
         <div className="app">
@@ -38,7 +50,8 @@ const App = () => {
                 </input>
                 <img 
                 alt='search'
-                onClick={()=> searchMovies(searchterm)}
+               // onClick={()=> searchMovies(searchterm)}
+                //onClick={handleClick}
                 />
             </div>
            
